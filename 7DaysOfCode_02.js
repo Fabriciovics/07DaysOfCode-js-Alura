2a5278d19a68ef93d08a9562d7d493c5c9f20ce9
unknown <fabriciovics@gmail.com>
2023-01-16 09:28:05 -0300

    Alterando Alert exercicio 02

--- 7DaysOfCode_02.js
+++ 7DaysOfCode_02.js
@@ -1,13 +1,16 @@
-let nome = prompt("Digite seu nome");
-let idade = prompt("Digite sua idade");
-let linguagem = prompt("Qual linguagem de programação você está estudando?");
+const nome = prompt("Digite seu nome");
+const idade = prompt("Digite sua idade");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
 
-console.log("Olá, " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem);
+const msg = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"!`;
 
-let pergunta = prompt("Você gosta de estudar " + linguagem + " ? Responda com o número 1 para SIM ou 2 para NÃO.");
+alert(msg);
+
+const pergunta = prompt("Você gosta de estudar " + linguagem + " ? Responda com o número 1 para SIM ou 2 para NÃO.");
 
 if (pergunta == '1') {
-  console.log("Muito bom! Continue estudando e você terá muito sucesso.");
+  alert("Muito bom! Continue estudando e você terá muito sucesso.");
 } else {
-  console.log("Ahh que pena... Já tentou aprender outras linguagens?");
+  ñ
+  alert("Ahh que pena... Já tentou aprender outras linguagens?");
 }
