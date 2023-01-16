cb367c228ad8f417574ab22ab7e21e07bdc1c6a9
unknown <fabriciovics@gmail.com>
2023-01-16 09:18:34 -0300

    Adicionando desafio 01 e 02

---
+++ 7DaysOfCode_02.js
@@ -0,0 +1,13 @@
+let nome = prompt("Digite seu nome");
+let idade = prompt("Digite sua idade");
+let linguagem = prompt("Qual linguagem de programação você está estudando?");
+
+console.log("Olá, " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem);
+
+let pergunta = prompt("Você gosta de estudar " + linguagem + " ? Responda com o número 1 para SIM ou 2 para NÃO.");
+
+if (pergunta == '1') {
+  console.log("Muito bom! Continue estudando e você terá muito sucesso.");
+} else {
+  console.log("Ahh que pena... Já tentou aprender outras linguagens?");
+}
